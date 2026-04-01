b374e57a02865d4a0a3d9b9777a748707523d150
apip2pipp <afifkhosyidzaki@gmail.com>
2026-04-01 21:58:49 +0700

    Refactor GoogleAuthController to remove unused RedirectResponse import

--- avenution/app/Http/Controllers/Auth/GoogleAuthController.php
+++ avenution/app/Http/Controllers/Auth/GoogleAuthController.php
@@ -5,12 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class GoogleAuthController extends Controller
 {
