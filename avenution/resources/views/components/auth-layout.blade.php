c8911d713f4ade2836e4456f4ce3ffc568b58f6c
apip2pipp <afifkhosyidzaki@gmail.com>
2026-04-08 14:15:13 +0700

    Add favicon link for Avenution application

--- avenution/resources/views/components/auth-layout.blade.php
+++ avenution/resources/views/components/auth-layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" type="image/png" href="{{ asset('images/icon_Avenution.png') }}">
 
         <title>{{ config('app.name', 'Avenution') }} - {{ $title ?? 'Sign In' }}</title>
 
