6f42a5ae09522c6cf92807c239e7e630ec173840
apip2pipp <afifkhosyidzaki@gmail.com>
2026-04-28 09:59:04 +0700

    fix: set default value for trusted proxies to allow forwarded HTTPS

--- avenution/app/Http/Middleware/TrustProxies.php
+++ avenution/app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,8 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    // Trust reverse proxies (ngrok, load balancers) so forwarded HTTPS is respected.
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
